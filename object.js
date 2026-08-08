87e8f9b44666af5437dcb082d1be9c969adcc3fc
Darbychic <darbychic26@gmail.com>
2026-08-08 12:12:09 +0100

    added objects

---
+++ object.js
@@ -0,0 +1,41 @@
+const obj = {
+    name : "Ayodele Deborah Oluwaseun",
+    gender: "non-binary",
+    age: 30,    
+    complexion: "dark",
+};
+
+let phone = {
+    brand: "samsung",
+    model: "Galaxy S22",
+    price: 350000
+};
+console.log(phone);
+
+phone.color = "black";
+console.log(phone);
+let student = {
+    name: "Godwin",
+    regNo: "C8/001",
+    course: "Javascript",
+    year: 2024,
+    friends : ["john", "jane", "doe"]
+}
+//adding to the object
+console.log(student);
+student.gender = "Male";
+console.log(student);
+student.course = "Node.js";
+console.log(student);
+delete student.year;
+console.log(student);
+
+//destructuring
+let employee = {        
+    name : "Tunde Balogun",
+    role : "Frontend Developer",
+    salary : 450000,
+    city : "Lagos",
+};
+const {name, role, salary, city} = employee;                
+
